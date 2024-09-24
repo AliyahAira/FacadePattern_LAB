33fdbc41d59bd4117392544edf8c9c5673d15979
Nekoayah <133284929+AliyahAira@users.noreply.github.com>
2024-09-24 01:05:00 +0000

    Lab Seatwork - Facade Pattern

---
+++ TV.java
@@ -0,0 +1,12 @@
+public class TV implements HomeService {
+
+    public void turnOn() {
+        System.out.println("Tv is ON... ✔️ ");
+
+    }
+
+    public void turnOff() {
+        System.out.println("Tv is OFF... ❌ ");
+    }
+
+}
